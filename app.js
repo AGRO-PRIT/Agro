13161aa816a8a31ad06d5c8c387d9b168f578d28
Amanda <amanda.novais2401@gmail.com>
2024-08-02 14:24:45 -0300

    arrumando node

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 const port = 3000;
 
-app.use(express.static("app/public"));
+app.use(express.static('app/public'));
 
-app.set("view engine", "ejs");
-app.set("views", "./app/views");
+app.set('view engine', 'ejs');
+app.set('views', './app/views');
 
 app.use(express.json()); 
 app.use(express.urlencoded({ extended: true }));
 
-var rotas = require("./app/routes/router");
-app.use("/", rotas);
+var rotas = require('./app/routes/router');
+app.use('/', rotas);
 
 
 app.listen(port, () => {
