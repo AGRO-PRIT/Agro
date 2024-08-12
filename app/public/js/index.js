7a847317cd4c4009cdbdfb1d068ca7352a2dcf39
kawan-lima <kawan272845@gmail.com>
2024-08-12 01:51:10 -0300

    mexendo no css

--- app/public/js/index.js
+++ app/public/js/index.js
@@ -1,2 +1,26 @@
 
+/*Slider JS*/
 
+    let slideIndex = 0;
+
+    /*funçao para exibir o slide*/
+
+    function showSlides() {
+    const slides = document.querySelectorAll('.slide');
+    slideIndex++;
+
+    if (slideIndex >= slides.length) {
+        slideIndex = 0;
+    }
+
+    /*movimento dos slides*/
+    const offset = slideIndex * -100;
+    document.querySelector('.slides').style.transform = `translateX(-${slideIndex * 100}%)`;
+
+    /* Looping a cada 3 segundos */
+
+    setTimeout(showSlides, 3000);
+}
+
+/*inicialização dos slides*/
+    showSlides();
