d86364bb3951e5582502633c30b91498b1aea9fa
kawan-lima <kawan272845@gmail.com>
2024-10-24 02:42:45 -0300

    save carrosel Agricultores

--- app/public/js/carrosel3.js
+++ app/public/js/carrosel3.js
@@ -1,27 +1,36 @@
+const carousel3 = document.querySelector('.carousel-container3');
+const indicators3 = document.querySelectorAll('.indicator3');
+const prevBtn3 = document.querySelector('.prev3');
+const nextBtn3 = document.querySelector('.next3');
 
-const prev3 = document.querySelector('.prev3');
-const next3 = document.querySelector('.next3');
-const productContainer = document.querySelector('.carousel-container3');
-
-// Definir o índice inicial e o número de itens visíveis
 let currentIndex3 = 0;
-const itemWidth = 1250; // Largura de cada item incluindo margem
-const itemsToShow = 1; // Quantidade de itens visíveis no carrossel
-const totalItems = document.querySelectorAll('.carousel-item3').length;
-const maxIndex = Math.ceil(totalItems / itemsToShow) - 1; // Número máximo de páginas
 
-// Função para mover o carrossel para a direita
-next3.addEventListener('click', () => {
-    if (currentIndex3 < maxIndex) {
-        currentIndex3++;
-        productContainer.style.transform = `translateX(-${currentIndex3 * (itemWidth * itemsToShow)}px)`;
-    } console.log("testeprev");
+function updateCarousel3() {
+    carousel3.style.transform = `translateX(-${currentIndex3 * 1300}px)`;
+    indicators3.forEach((indicator3, index) => {
+        if (index === currentIndex3) {
+            indicator3.classList.add('active');
+        } else {
+            indicator3.classList.remove('active');  
+        }
+    });
+}
+
+nextBtn3.addEventListener('click', () => {
+    currentIndex3 = (currentIndex3 + 1) % indicators3.length;
+    updateCarousel3();
 });
 
-// Função para mover o carrossel para a esquerda
-prev3.addEventListener('click', () => {
-    if (currentIndex3 > 0) {
-        currentIndex3--;
-        productContainer.style.transform = `translateX(-${currentIndex3 * (itemWidth * itemsToShow)}px)`;
-    } console.log("testeprev");
+prevBtn3.addEventListener('click', () => {
+    currentIndex3 = (currentIndex3 - 1 + indicators3.length) % indicators3.length;
+    updateCarousel3();
 });
+
+indicators3.forEach((indicator3, index) => {
+    indicator3.addEventListener('click', () => {
+        currentIndex3 = index;
+        updateCarousel3();
+    });
+});
+
+updateCarousel3();
