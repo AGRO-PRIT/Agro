678e91f5b08682300b82816f0d9eb9c929cb6048
Amanda <amanda.cunha@fieb.edu.br>
2024-10-24 10:10:28 -0300

    carrossel vendedor correção

--- app/public/js/carrosel3.js
+++ app/public/js/carrosel3.js
@@ -1,26 +1,27 @@
-const prevBtn3 = document.querySelector('.prev3');
-const nextBtn3 = document.querySelector('.next3');
-const productContainer = document.querySelector('.todos-produtos');
+
+const prev3 = document.querySelector('.prev3');
+const next3 = document.querySelector('.next3');
+const productContainer = document.querySelector('.carousel-container3');
 
 // Definir o índice inicial e o número de itens visíveis
 let currentIndex3 = 0;
-const itemWidth = 190; // Largura de cada item incluindo margem
-const itemsToShow = 4; // Quantidade de itens visíveis no carrossel
-const totalItems = document.querySelectorAll('.box-produto').length;
+const itemWidth = 1250; // Largura de cada item incluindo margem
+const itemsToShow = 1; // Quantidade de itens visíveis no carrossel
+const totalItems = document.querySelectorAll('.carousel-item3').length;
 const maxIndex = Math.ceil(totalItems / itemsToShow) - 1; // Número máximo de páginas
 
 // Função para mover o carrossel para a direita
-nextBtn3.addEventListener('click', () => {
+next3.addEventListener('click', () => {
     if (currentIndex3 < maxIndex) {
         currentIndex3++;
         productContainer.style.transform = `translateX(-${currentIndex3 * (itemWidth * itemsToShow)}px)`;
-    }
+    } console.log("testeprev");
 });
 
 // Função para mover o carrossel para a esquerda
-prevBtn3.addEventListener('click', () => {
+prev3.addEventListener('click', () => {
     if (currentIndex3 > 0) {
         currentIndex3--;
         productContainer.style.transform = `translateX(-${currentIndex3 * (itemWidth * itemsToShow)}px)`;
-    }
+    } console.log("testeprev");
 });
